035d7963946e288785f241dadf1479441a29e98c
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-09-18 03:02:21 -0400

    Adding bunch of shit

--- app.js
+++ app.js
@@ -1,30 +1,119 @@
-var BTSP = require('bluetooth-serial-port');
-var serial = new BTSP.BluetoothSerialPort();
+bluetoothAddress = '20-16-05-25-41-99';
 
-var ADDRESS = '78-40-84-ab-ce-b0'
+var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 
-serial.on('found', function(address, name) {
+// all times in ms
+const REFRESHRATE = 20;
+const TRIGGERVAL = 1000;
+const TAPTRIGGERTIME = 500;
+const IGNORETIME = 100;
+const BUFFERTIME = 2000;
 
-  // you might want to check the found address with the address of your
-  // bluetooth enabled Arduino device here.
-  console.log(address, name);
+var rawRolling1 = Array(BUFFERTIME/REFRESHRATE).fill(1023);
+var rawRolling2 = Array(BUFFERTIME/REFRESHRATE).fill(1023);
 
-  serial.findSerialPortChannel(address, function(channel) {
-    console.log(channel)
-    serial.connect(ADDRESS, channel, function() {
-          console.log('connected');
-          process.stdin.resume();
-          process.stdin.setEncoding('utf8');
-          console.log(
-              'Press "1" or "0" and "ENTER" to turn on or off the light.')
+var counter = 0;
+var maxCounter = 20;
+var under = false;
+var drag = true;
 
-          process.stdin.on('data', function(data) { serial.write(data); });
+function checkTap() {
+  var currentValue = rawRolling1[rawRolling1.length - 1]
+  var pastValue = rawRolling1[rawRolling1.length - 2]
+  console.log(currentValue, counter)
+  if(currentValue > TRIGGERVAL && under === false) {
+    console.log("Nothing");
+    return; //nothing is happening
+  }
 
-          serial.on('data',
-                    function(data) { console.log('Received: ' + data); });
-        },
-        function() { console.log('cannot connect'); });
-  });
+  if(currentValue > TRIGGERVAL && under === true && counter < maxCounter) {
+    console.log("!!!!!!!This is a click~~~~~");
+    under = false;
+    counter = 0;
+    return;
+  }
+
+  if(currentValue < TRIGGERVAL && drag === true) {
+    if(currentValue - pastValue < 0) {
+      console.log("up");
+    } else if(currentValue - pastValue > 0) {
+      console.log("down");
+    }
+  }
+
+  if(currentValue > TRIGGERVAL && under === true && counter >= maxCounter) {
+    under = false //end of drag;
+    counter = 0;
+    return;
+  }
+
+  if (currentValue < TRIGGERVAL && under === false) {
+    console.log("Just went under");
+    under = true; // starting a click/drag?
+    return;
+  }
+
+  if(currentValue < TRIGGERVAL && under === true && counter < maxCounter) {
+    console.log("Still waiting");
+    counter++; //still waiting to see if it is a click
+    return;
+  }
+
+  if(currentValue < TRIGGERVAL && under === true && counter >= maxCounter) {
+    console.log("Now it's a drag");
+    drag = true; //now it's a drag
+    return;
+  }
+
+  console.log("something else?");
+  under = false;
+  drag = false;
+  counter = 0;
+}
+
+var rawSerialString = '';
+btSerial.on('found', function(address, name) {
+  if(address === bluetoothAddress) {
+    process.stdout.write('Arduino found, connecting... ');
+    btSerial.findSerialPortChannel(address, function(channel) {
+        btSerial.connect(address, channel, function() {
+            console.log('Done!');
+
+            btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
+                if (err) console.log(err);
+            });
+
+            btSerial.on('data', function(buffer) {
+                rawSerialString += buffer.toString('utf-8');
+
+                if (!rawSerialString.includes(']')) {
+                  return;
+                }
+
+                var rawData;
+                try {
+                  rawData = JSON.parse(rawSerialString);
+                  //console.log(rawData[0]);
+
+                  rawRolling1.push(rawData[0]);
+                  rawRolling1.shift();
+
+                  checkTap();
+                } catch (e) {
+                  rawSerialString = '';
+                   //console.log('TRANSMISSION ERROR');
+                }
+            });
+        }, function () {
+            console.log('Connection Error Type 1');
+        });
+
+        // close the connection when you're ready
+        btSerial.close();
+    }, function() {
+        console.log('Connection Error Type 2');
+    });
+  }
 });
 
-serial.inquire();
+btSerial.inquire();
---
+++ src/bluetooth.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+// This code uses the SoftwareSerial library.
+// It can be obtained here: http://arduino.cc/en/Reference/SoftwareSerial
+
+unsigned int timeout = 0;
+unsigned char state = 0;
+
+char val;        // variable to receive data from the serial port
+int ledpin = 13; // LED connected to pin 13
+
+void cleantime();
+// Timer2 service
+ISR(TIMER2_OVF_vect) {
+  TCNT2 = 0;
+  timeout++;
+  if (timeout > 61) {
+    state = 1;
+    timeout = 0;
+  }
+}
+
+// initialize the timer 2 service
+void init_timer2(void) {
+  TCCR2A |= (1 << WGM21) | (1 << WGM20);
+  TCCR2B |= 0x07;     // by clk/1024
+  ASSR |= (0 << AS2); // Use internal clock - external clock not used in Arduino
+  TIMSK2 |= 0x01;     // Timer2 Overflow Interrupt Enable
+  TCNT2 = 0;
+  sei();
+}
+
+// sets up the program
+void setup() {
+  // open the serial port
+  Serial.begin(9600);
+
+  // bind the ledpin as output
+  pinMode(ledpin, OUTPUT);
+
+  // bind pin 2 as input
+  pinMode(2, INPUT);
+
+  // interrupt for reading from the bluetooth connection
+  attachInterrupt(0, cleantime, FALLING);
+  init_timer2();
+}
+
+// function for controlling the led
+void control(void) {
+  int sensor1 = analogRead(A0);
+  Serial.print("[");
+  Serial.print(sensor1);
+  Serial.println("]");
+}
+
+// control loop for the program
+void loop() {
+  control();
+  delay(20);
+}
+
+void cleantime() {
+  timeout = 0;
+  state = 0;
+}
+
+// void serialEvent() {
+//   while (Serial.available()) {
+//     char inChar = (char)Serial.read();
+//     Serial.println(inChar);
+//   }
+// }
