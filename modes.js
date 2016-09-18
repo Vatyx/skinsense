1282332640d3dbd41237b16dab17e46d7bc1c368
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-09-18 05:12:56 -0400

    Added game commands

--- modes.js
+++ modes.js
@@ -7,39 +7,61 @@ const robot = require('robotjs');
 function modeHandler(mode, action) {
 	switch (mode) {
 		case 'audio':
-			audioHandler(action)
+			audioHandler(action);
 			break;
 		case 'video':
-			console.log('Video')
+			console.log('Video');
+			break;
+		case 'game':
+			arrowsHandler(action);
 			break;
 		default:
 			console.error('Unhandled mode case');
 	}
 }
 
+function arrowsHandler(action) {
+	switch (action) {
+		case 'flick-up':
+			robot.keyTap('up');
+			break;
+		case 'flick-down':
+			robot.keyTap('down');
+			break;
+		case 'flick-left':
+			robot.keyTap('left');
+			break;
+		case 'flick-right':
+			robot.keyTap('right');
+			break;
+		default:
+			console.error('Unhandled arrow action');
+	}
+}
+
 function audioHandler(action){
 	switch (action) {
-		case 'toggle-play':
+		case 'tap': // Toggle playing of music
 			toggleAudio();
 			break;
-		case 'next':
+		case 'flick-right':
 			robot.keyTap('audio_next');
 			break;
-		case 'prev':
+		case 'flick-left':
 			robot.keyTap('audio_prev');
 			break;
-		case 'volume-up':
+		case 'slide-up':
 			robot.keyTap('audio_vol_up');
 			break;
-		case 'volume-down':
+		case 'slide-down':
 			robot.keyTap('audio_vol_down');
 			break;
 		default:
-			console.error('Unhandled audio action')
+			console.error('Unhandled audio action');
 	}
 }
 
 function toggleAudio() {
-	return robot.keyTap('audio_pause')
+	return robot.keyTap('audio_pause');
 }
 
